f4c2d7179b71528857d1a59aa095638e57cd01d7
sborland <sborland@ucsc.edu>
2015-01-10 15:07:09 -0800

    Added Ranking System with working end screen

--- src/app.js
+++ src/app.js
@@ -11,25 +11,64 @@ var Vector2 = require('vector2');
 var steps = 0;
 var monsterSteps =-50;
 var death =false;
+var rankCheck = [
+  {
+    check:5,
+    rank:'Rank F: Monster Meat'
+  },
+  {
+   check:10,
+    rank:'Rank D: First to Fall'
+  },
+  {
+   check:15,
+   rank:'Rank C: Average Survivor' 
+  },
+  {
+    check:20,
+    rank:'Rank B: Apocalypse Leader'
+  },
+  {
+    check:25,
+    rank:'Rank A: Monster Overlord'
+  },
+  {
+    check:30,
+    rank:'Rank S: Nightmare Conquerer'
+  },
+];
+
 var runnerSprite = [
   {
-   position: new Vector2(-10,60),
+   position: new Vector2(-10,50),
    size: new Vector2(80,35),
   image: 'images/runner1.png' 
   },
   {
-   position: new Vector2(-10,60),
+   position: new Vector2(-10,50),
    size: new Vector2(80,35),
   image: 'images/runner2.png' 
   },
 ];
+/*var zombieSprite = [
+  {
+   position: new Vector2(60,60),
+   size: new Vector2(80,35),
+  image: 'images/zombie1.png' 
+  },
+  {
+ position: new Vector2(60,60),
+  size: new Vector2(80,35),
+ image: 'images/zombie2.png' 
+  },
+];*/
 
 
 var main = new UI.Card({
   title: 'Monster Track',
-  icon: 'images/photo.png',
+  icon: 'images/logo.png',
   subtitle: 'Ready to Run!',
-  body:  'Team Dont Byte Me'
+  body:  'Brought to you by Team Dont Byte Me!'
 });
 
 main.show();
@@ -70,22 +109,29 @@ main.on('click', 'up', function(e) {
     runWind.add(bgRect);
   
   ////////////////Sprite Images on Screen////////////////// 
-  var deathP = new UI.Image({
-    position: new Vector2(0,0),
-    size: new Vector2(144,160),
-    image: 'images/werewolfdead.png'
-  });  
+   var zombiepic = new UI.Image({
+    position: new Vector2(50,50),
+   size: new Vector2(80,35),
+  image: 'images/zombie1.png' 
+    }); 
+   var runnerpic = new UI.Image({
+  position: new Vector2(-10,50),
+   size: new Vector2(80,35),
+  image: 'images/runner1.png' 
+  }); 
+  runWind.add(runnerpic); 
+    runWind.add(zombiepic); 
   
   
     runWind.add(botRect);
     runWind.add(topRect);
     runWind.add(stepsDisplay);
-    runWind.show();
 
 ///////////////////Human Step Counter/////////////
   Accel.on('tap', function(e) {
-  //  if (death === true){
-   // }else {
+    if (death === true){
+   // runWind.add(deathP);
+    } else {
     if (e.direction > 0){
       didStep = true;
       }else{
@@ -119,11 +165,15 @@ main.on('click', 'up', function(e) {
       runWind.remove(stepsDisplay);
       steps += 1;
       runWind.add(stepsDisplay);
-      didStep = false;
+      didStep = false;  
     }
-       runWind.show();
-  //  }
-  });
+  //  if (death === true){
+  // runWind.add(deathP);
+   // }
+  }
+console.log('accel death?: '+death);
+//console.log('accel onscreen: '+runWind.index(deathP));
+});
   
 /////////////////Monster Step Counter//////////////////////
  function monster(){
@@ -131,7 +181,7 @@ main.on('click', 'up', function(e) {
   var monsterSpeed = Math.floor(Math.random() * (5-1) + 1);
   monsterSteps = monsterSteps+monsterSpeed; //i.e -500+1=-499
   monsterStepsDisplay = steps-monsterSteps; //i.e 0-(-499)=499   
-  
+   
   //updates monster display 
   runWind.remove(botRect);
   runWind.add(botRect);
@@ -148,13 +198,34 @@ main.on('click', 'up', function(e) {
    
    //Death 
     if (monsterStepsDisplay <= 0){
+      death = true;
       clearInterval(monTimer);
-      runWind.add(deathP);
+      deathscreen();
     }
-    runWind.show();
-   console.log('human steps: '+steps+' -monstersteps: '+monsterSteps);
-   console.log('monster steps display: '+ monsterStepsDisplay); 
- }   
+  
+ //  console.log('monster onscreen: '+runWind.index(deathP));
+   
+ }  
+ runWind.show();
+
+//function when you get caught
+ function deathscreen(){
+   var rankStr;
+   for (var i =0; i <= 5; i++){
+     if (steps>=rankCheck[i].check){
+       rankStr = rankCheck[i].rank;
+     }
+   }
+   console.log('ranking: '+rankStr);
+   var deathCard = new UI.Card({
+     title:'Ran '+steps+' steps!',
+     body:rankStr,
+     banner: 'images/Zdeathscreen.png'
+   });
+   runWind.hide();
+   deathCard.show();
+ }
+
 });
 
 
