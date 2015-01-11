1f029f417fee9652bbcdcbd0a3604be77a307875
sborland <sborland@ucsc.edu>
2015-01-10 16:28:31 -0800

    Every feature implemented! Beta Testing is ago!

--- src/app.js
+++ src/app.js
@@ -13,28 +13,28 @@ var monsterSteps =-50;
 var death =false;
 var rankCheck = [
   {
+    check:0,
+    rank:'Rank F:Monster Meat'
+  },
+  {
+   check:2,
+    rank:'Rank D:First Fallen'
+  },
+  {
+   check:3,
+   rank:'Rank C:Last Survivor' 
+  },
+  {
+    check:4,
+    rank:'Rank B:The King'
+  },
+  {
     check:5,
-    rank:'Rank F: Monster Meat'
+    rank:'Rank A:The Overlord'
   },
   {
-   check:10,
-    rank:'Rank D: First to Fall'
-  },
-  {
-   check:15,
-   rank:'Rank C: Average Survivor' 
-  },
-  {
-    check:20,
-    rank:'Rank B: Apocalypse Leader'
-  },
-  {
-    check:25,
-    rank:'Rank A: Monster Overlord'
-  },
-  {
-    check:30,
-    rank:'Rank S: Nightmare Conquerer'
+    check:6,
+    rank:'Rank S:The Conquerer'
   },
 ];
 
@@ -79,7 +79,7 @@ main.on('click', 'up', function(e) {
   Accel.init();
   var didStep = false;
   var monsterStepsDisplay = 10;
-  var monTimer = setInterval(function () {monster()}, 1000);
+  var monTimer = setInterval(function () {monster()}, 5000);
 
   var runWind = new UI.Window();
   
@@ -178,7 +178,7 @@ console.log('accel death?: '+death);
 /////////////////Monster Step Counter//////////////////////
  function monster(){
   //updates monster stats
-  var monsterSpeed = Math.floor(Math.random() * (5-1) + 1);
+  var monsterSpeed = Math.floor(Math.random() * (3-1) + 1);
   monsterSteps = monsterSteps+monsterSpeed; //i.e -500+1=-499
   monsterStepsDisplay = steps-monsterSteps; //i.e 0-(-499)=499   
    
@@ -202,8 +202,7 @@ console.log('accel death?: '+death);
       clearInterval(monTimer);
       deathscreen();
     }
-  
- //  console.log('monster onscreen: '+runWind.index(deathP));
+
    
  }  
  runWind.show();
@@ -216,11 +215,11 @@ console.log('accel death?: '+death);
        rankStr = rankCheck[i].rank;
      }
    }
-   console.log('ranking: '+rankStr);
+   //console.log('ranking: '+rankStr);
    var deathCard = new UI.Card({
      title:'Ran '+steps+' steps!',
      body:rankStr,
-     banner: 'images/Zdeathscreen.png'
+     banner: 'images/Zdeathscreen.png',
    });
    runWind.hide();
    deathCard.show();
@@ -230,24 +229,301 @@ console.log('accel death?: '+death);
 
 
 
-
+/////////WereWolf/////////
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
+  Accel.init();
+  var didStep = false;
+  var monsterStepsDisplay = 10;
+  var monTimer = setInterval(function () {monster()}, 3000);
+
+  var runWind = new UI.Window();
+  
+  var topRect = new UI.Rect({
+    position: new Vector2(10,5),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+  var botRect = new UI.Rect({
+    position: new Vector2(10,110),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+  var bgRect = new UI.Rect({
+    position: new Vector2(0,0),
+    size: new Vector2(144,168),
+    backgroundColor:'white'
+    });
+  var stepsDisplay = new UI.Text({
+    position: new Vector2(10,5),
+    size: new Vector2(124,20),
+    text: 'START RUNNING!',
+    color: 'white',
     textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
+    });
+    
+    runWind.add(bgRect);
+  
+  ////////////////Sprite Images on Screen////////////////// 
+   var werewolfpic = new UI.Image({ ///CHANGE
+    position: new Vector2(50,50),
+   size: new Vector2(80,35),
+  image: 'images/were1.png' 
+    }); 
+   var runnerpic = new UI.Image({
+  position: new Vector2(-10,50),
+   size: new Vector2(80,35),
+  image: 'images/runner1.png' 
+  }); 
+  runWind.add(runnerpic); 
+    runWind.add(werewolfpic); ///CHANGE
+  
+  
+    runWind.add(botRect);
+    runWind.add(topRect);
+    runWind.add(stepsDisplay);
+
+///////////////////Human Step Counter/////////////
+  Accel.on('tap', function(e) {
+    if (death === true){
+   // runWind.add(deathP);
+    } else {
+    if (e.direction > 0){
+      didStep = true;
+      }else{
+      didStep = true;
+    }
+    var runnerpic = new UI.Image({
+    position: runnerSprite[1].position,
+    size: runnerSprite[1].size,
+    image: runnerSprite[(steps%2)].image,
+    }); 
+    runWind.add(runnerpic);  
+
+    //Step counter display settings
+     var topRect = new UI.Rect({
+    position: new Vector2(10,5),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+    runWind.add(topRect);
+  
+    var stepsDisplay = new UI.Text({
+      position: new Vector2(10,5),
+      size: new Vector2(124,20),
+      text: 'you ran '+(steps+1)+' steps!',
+      color: 'white',
+      textAlign: 'center'
+      }); 
+   
+    //removes old step display and replaces it with new display
+    if (didStep === true){ 
+      runWind.remove(stepsDisplay);
+      steps += 1;
+      runWind.add(stepsDisplay);
+      didStep = false;  
+    }
+  }
+});
+  
+/////////////////Monster Step Counter//////////////////////
+ function monster(){
+  //updates monster stats
+  var monsterSpeed = Math.floor(Math.random() * (5-1) + 1);
+  monsterSteps = monsterSteps+monsterSpeed; //i.e -500+1=-499
+  monsterStepsDisplay = steps-monsterSteps; //i.e 0-(-499)=499   
+   
+  //updates monster display 
+  runWind.remove(botRect);
+  runWind.add(botRect);
+    var monsterDisplay = new UI.Text({
+      position: new Vector2(10,110),
+      size: new Vector2(124,20),
+      font: 'gothic-14',
+      text: 'scary werewolf is '+monsterStepsDisplay+' steps away!',
+      color: 'white',
+      textAlign: 'center'
+      }); 
+    runWind.remove(monsterDisplay);
+    runWind.add(monsterDisplay);
+   
+   //Death 
+    if (monsterStepsDisplay <= 0){
+      death = true;
+      clearInterval(monTimer);
+      steps = 0;
+      monsterSteps =-50;
+      death =false;
+      deathscreen();
+    } 
+ }  
+ runWind.show();
+
+//function when you get caught
+ function deathscreen(){
+   var rankStr;
+   for (var i =0; i <= 5; i++){
+     if (steps>=rankCheck[i].check){
+       rankStr = rankCheck[i].rank;
+     }
+   }
+   //console.log('ranking: '+rankStr);
+   var deathCard = new UI.Card({
+     title:'Ran '+steps+' steps!',
+     body:rankStr,
+     banner: 'images/werewolfdead1.png'///CHANGE
+   });
+   runWind.hide();
+   deathCard.show();
+ }
+
 });
 
+/////////Vampire/////////
 main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  Accel.init();
+  var didStep = false;
+  var monsterStepsDisplay = 10;
+  var monTimer = setInterval(function () {monster()}, 1000);
+
+  var runWind = new UI.Window();
+  
+  var topRect = new UI.Rect({
+    position: new Vector2(10,5),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+  var botRect = new UI.Rect({
+    position: new Vector2(10,110),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+  var bgRect = new UI.Rect({
+    position: new Vector2(0,0),
+    size: new Vector2(144,168),
+    backgroundColor:'white'
+    });
+  var stepsDisplay = new UI.Text({
+    position: new Vector2(10,5),
+    size: new Vector2(124,20),
+    text: 'START RUNNING!',
+    color: 'white',
+    textAlign: 'center'
+    });
+    
+    runWind.add(bgRect);
+  
+  ////////////////Sprite Images on Screen////////////////// 
+   var vampirepic = new UI.Image({ ///CHANGE
+    position: new Vector2(50,60),
+    size: new Vector2(100,30),
+    image: 'images/vampire1.png' 
+    }); 
+   var runnerpic = new UI.Image({
+  position: new Vector2(-10,50),
+   size: new Vector2(80,35),
+  image: 'images/runner1.png' 
+  }); 
+  runWind.add(runnerpic); 
+    runWind.add(vampirepic); ///CHANGE
+  
+  
+    runWind.add(botRect);
+    runWind.add(topRect);
+    runWind.add(stepsDisplay);
+
+///////////////////Human Step Counter/////////////
+  Accel.on('tap', function(e) {
+    if (death === true){
+   // runWind.add(deathP);
+    } else {
+    if (e.direction > 0){
+      didStep = true;
+      }else{
+      didStep = true;
+    }
+    var runnerpic = new UI.Image({
+    position: runnerSprite[1].position,
+    size: runnerSprite[1].size,
+    image: runnerSprite[(steps%2)].image,
+    }); 
+    runWind.add(runnerpic);  
+
+    //Step counter display settings
+     var topRect = new UI.Rect({
+    position: new Vector2(10,5),
+    size: new Vector2(124,30),
+    backgroundColor:'black'
+    });
+    runWind.add(topRect);
+  
+    var stepsDisplay = new UI.Text({
+      position: new Vector2(10,5),
+      size: new Vector2(124,20),
+      text: 'you ran '+(steps+1)+' steps!',
+      color: 'white',
+      textAlign: 'center'
+      }); 
+   
+    //removes old step display and replaces it with new display
+    if (didStep === true){ 
+      runWind.remove(stepsDisplay);
+      steps += 1;
+      runWind.add(stepsDisplay);
+      didStep = false;  
+    }
+  }
 });
+  
+/////////////////Monster Step Counter//////////////////////
+ function monster(){
+  //updates monster stats
+  var monsterSpeed = Math.floor(Math.random() * (5-1) + 1); // CHANGE
+  monsterSteps = monsterSteps+monsterSpeed; //i.e -500+1=-499
+  monsterStepsDisplay = steps-monsterSteps; //i.e 0-(-499)=499   
+   
+  //updates monster display 
+  runWind.remove(botRect);
+  runWind.add(botRect);
+    var monsterDisplay = new UI.Text({
+      position: new Vector2(10,110),
+      size: new Vector2(124,20),
+      font: 'gothic-14',
+      text: 'spooky vampire is '+monsterStepsDisplay+' steps away!',
+      color: 'white',
+      textAlign: 'center'
+      }); 
+    runWind.remove(monsterDisplay);
+    runWind.add(monsterDisplay);
+   
+   //Death 
+    if (monsterStepsDisplay <= 0){
+      death = true;
+      clearInterval(monTimer);
+      steps = 0;
+      monsterSteps =-50;
+      death =false;
+      deathscreen();
+    } 
+ }  
+ runWind.show();
+
+//function when you get caught
+ function deathscreen(){
+   var rankStr;
+   for (var i =0; i <= 5; i++){
+     if (steps>=rankCheck[i].check){
+       rankStr = rankCheck[i].rank;
+     }
+   }
+   //console.log('ranking: '+rankStr);
+   var deathCard = new UI.Card({
+     title:'Ran '+steps+' steps!',
+     body:rankStr,
+     banner: 'images/vampiredead2.png'///CHANGE
+   });
+   runWind.hide();
+   deathCard.show();
+ }
+
+});
+
